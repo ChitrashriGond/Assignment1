600933bb00052f17288b93ed4e58c7372e4884cb
Chitrashri <chitrashri.xworkz@gmail.com>
2021-07-29 12:43:43 +0530

    java

---
+++ apptitude/src/com/xworkz/apptitude/task1/ReplaceChar.java
@@ -0,0 +1,12 @@
+package com.xworkz.apptitude.task1;
+
+public class ReplaceChar {
+
+	public static void main(String[] args) {
+		String s1="Hi im Chitrbshri";  
+		String replaceString=s1.replace('b','a');
+		System.out.println(replaceString); 
+
+	}
+
+}
---
+++ apptitude/src/com/xworkz/apptitude/task1/StringPelindrom.java
@@ -0,0 +1,18 @@
+package com.xworkz.apptitude.task1;
+
+import java.util.Scanner;
+
+public class StringPelindrom {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string :");
+		String str = sc.nextLine();
+		char[] charArr = str.toCharArray();
+		for (int i = 0; i < charArr.length; i++) {
+			
+		}
+	}
+
+}
